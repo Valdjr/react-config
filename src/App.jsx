3a4881a8f13700d07e0ea738c9dbdf7e1abb20cb
Valdjr <valdirdallagojr@gmail.com>
2021-05-10 20:56:28 -0300

    add fast refresh react

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./styles/global.scss";
 
 export function App() {
-	return <h1>Olá mundo</h1>;
+	return <h1>Olá mundo!</h1>;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,47 @@
-const path = require('path')
-const htmlwebpack = require('html-webpack-plugin')
+const path = require("path");
+const htmlwebpack = require("html-webpack-plugin");
+const fastrefresh = require("@pmmmwh/react-refresh-webpack-plugin");
 
-const isDev = process.env.NOVE_ENV !== 'production';
+const isDev = process.env.NOVE_ENV !== "production";
 
 module.exports = {
-    mode: isDev ? 'development' : 'production',
-    devtool: isDev ? 'eval-source-map' : 'source-map',
-    entry: path.resolve(__dirname, 'src', 'index.jsx'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname, 'public')
-    },
-    plugins: [
-        new htmlwebpack({
-            template: path.resolve(__dirname, 'public', 'index.html')
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            },
-            {
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-            },
-        ]
-    }
-}
+	mode: isDev ? "development" : "production",
+	devtool: isDev ? "eval-source-map" : "source-map",
+	entry: path.resolve(__dirname, "src", "index.jsx"),
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js",
+	},
+	resolve: {
+		extensions: [".js", ".jsx"],
+	},
+	devServer: {
+		contentBase: path.resolve(__dirname, "public"),
+		hot: true,
+	},
+	plugins: [
+		isDev && new fastrefresh(),
+		new htmlwebpack({
+			template: path.resolve(__dirname, "public", "index.html"),
+		}),
+	].filter(Boolean),
+	module: {
+		rules: [
+			{
+				test: /\.jsx$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						plugins: [isDev && require.resolve("react-refresh/babel")].filter(Boolean),
+					},
+				},
+			},
+			{
+				test: /\.scss$/,
+				exclude: /node_modules/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+		],
+	},
+};
